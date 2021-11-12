b45bf260977230cdeabcc9e6cd595238feb86c28
isaacwiita <isaacwiita@gmail.com>
2021-11-12 00:34:29 -0500

    some ui for home tab

--- app/src/main/java/com/example/musicapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/musicapp/ui/home/HomeFragment.java
@@ -4,41 +4,63 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import com.example.musicapp.R;
 import com.example.musicapp.databinding.FragmentHomeBinding;
+import com.example.musicapp.models.Playlist;
+import com.example.musicapp.ui.history.HistoryFragment;
+import com.example.musicapp.ui.settings.SettingsFragment;
+import com.example.musicapp.ui.settings.SettingsPlaylistSpinnerAdapter;
+import com.example.musicapp.ui.settings.SettingsViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+    private Button button_rec;
 
-    binding = FragmentHomeBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        button_rec = v.findViewById(R.id.button_rec);
+
+        button_rec.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
             }
         });
-        return root;
+
+        return v;
     }
 
-@Override
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
+
+    // adapted from https://stackoverflow.com/questions/32700818/how-to-open-a-fragment-on-button-click-from-a-fragment-in-android
+    // not working
+    public void replaceFragment(Fragment someFragment) {
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(((ViewGroup)getView().getParent()).getId(), someFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
 }
